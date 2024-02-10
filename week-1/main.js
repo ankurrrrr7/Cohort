d92471365aa62b7b56394032554ec931c662e8dc
ankurrrrr7 <ankurjha956@gmail.com>
2024-02-10 18:22:13 +0530

    Js

---
+++ week-1/main.js
@@ -0,0 +1,21 @@
+// To count till 1000
+
+/*let  z=1000;
+for(let i=1; i<=z; i++){
+    console.log(i);
+}
+*/
+
+let male = false;
+let female = false;
+
+if(male==true){
+    
+    console.log("Hello");
+}
+else if(female==true){
+    console.log("Hey");
+}
+else{
+    console.log("Please enter your gender");
+}
