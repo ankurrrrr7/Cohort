dec78b00b7113b416a49f0b8059de0321155c65a
ankurrrrr7 <ankurjha956@gmail.com>
2024-08-05 15:23:56 +0530

    Callbacks

--- index.js
+++ index.js
@@ -37,16 +37,41 @@ const alluser =[{
     return sumvalue;
 
   }
-//CALLBACK FUNCTION
-  const value = sum(1,2);
-  console.log(value);
 
-  function sum(num1,num2){
-    const result = num1+num2;
-    return result;
-  }
-  function displayresult(data){
-    console.log("Result of the sum is",data);
-  }
-  const answere = sum(1,2)
-  displayresult(answere);
+
+//CALLBACK FUNCTION
+function sum1(num1, num2, funToCall){
+  const result = num1+num2;
+  funToCall(result);
+}
+function displayresult(data){
+  console.log("Result of the sum is",data);
+}
+function displayresultpassive(data){
+  console.log("Sum result is", data);  
+}
+const answere = sum1(1,2,displayresultpassive);
+
+//Example 2
+function arthemtic(a,b,arthemticFinal){
+  const sol =arthemticFinal(a,b);
+  return sol;
+
+}
+function sum(a,b){
+      return a+b;
+}
+function sub(a,b){
+      return a-b;
+}
+
+const solution = arthemtic(1,2,sum);
+console.log(solution);
+
+
+// timeout 
+function greet(){
+  console.log("Hello world");
+}
+
+ setTimeout(greet, 1*1000);
